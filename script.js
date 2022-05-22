92a8032341985cd6123c566e98b6cec3bd5af57e
Leiliane Ferreira <leilianeferreira53@gmail.com>
2022-05-22 13:57:07 -0300

    dreamSushi

---
+++ script.js
@@ -0,0 +1,19 @@
+function redirecionamentoInicio() {
+	window.location.href="index.html"; 
+}
+
+function redirecionamentoCardapio() {
+	window.location.href="cardapio.html"; 
+}
+
+function redrecionamentoInicio() {
+	window.location.href="index.html"; 
+}
+
+function abrir(id) {
+	document.getElementById(id).style.display = "block"; 
+}
+
+function fechar(id) {
+	document.getElementById(id).style.display = "none"; 
+}
